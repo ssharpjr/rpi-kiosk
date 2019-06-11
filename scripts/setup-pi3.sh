e93e12ea90ea329013040ac9c34bb5521b6f09ae
Stacey Sharp <ssharpjr@gmail.com>
2019-06-11 14:49:34 -0400

    updates

--- scripts/setup-pi3.sh
+++ scripts/setup-pi3.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# RPI Kiosk Installer for Raspberry Pi
+
+# Basic Raspberry Pi 3 setup script
 
 # Variables
 HOSTNAME_FILE="/boot/HOSTNAME.TXT"
@@ -7,11 +8,10 @@ APP_USER="runner"
 APP_USER_PW="tpirunner"
 MAINT_USER="pimaint"
 MAINT_USER_PW="tpimaint"
-REPO="rpi-kiosk"
 DEBUG=0  # Set to 1 for debugging
 
 
-echo -e "\nRPI Kiosk Installer\n"
+echo -e "\nSetting up Raspberry Pi\n"
 
 # Disable console screen saver during setup
 sudo setterm -blank 0
@@ -23,15 +23,6 @@ check_root() {
         echo "Try 'sudo bash $0'"
         exit 1
     fi
-}
-
-
-copy_scripts() {
-echo -e "Copying Scripts...\n"
-    # Copy Scripts
-    sudo cp boot/xinitrc /boot/xinitrc
-    sudo cp boot/HOSTNAME.TXT /boot/HOSTNAME.TXT
-    sudo cp boot/LINK.TXT /boot/LINK.TXT
 }
 
 
@@ -101,6 +92,39 @@ set_term() {
 }
 
 
+uninstall_packages() {
+    echo -n "Uninstalling Packages..."
+    sudo apt-get remove --purge wolfram-engine libreoffice* scratch \
+         minecraft-pi sonic-pi dillo gpicview oracle-java8-jdk \
+         openjdk-7-jre oracle-java7-jdk openjdk-8-jre vim-tiny -y
+    echo -e " Done.\n"
+}
+
+
+update_packages() {
+    echo -n "Updating Packages..."
+    sudo apt-get update && \
+    sudo apt-get upgrade -y && \
+    sudo apt-get dist-upgrade -y && \
+    sudo apt-get autoremove -y && \
+    sudo apt-get autoclean &&
+    sync
+    echo -e " Done.\n"
+}
+
+
+install_packages() {
+    echo -n "Installing Packages..."
+    sudo apt-get install -y --no-install-recommends vim matchbox xserver-xorg \
+                        xserver-xorg-legacy x11-xserver-utils \
+                        ttf-mscorefonts-installer xinit xwit sqlite3 \
+                        libnss3 chromium-browser git
+    sudo apt-get autoremove -y && sudo apt-get autoclean && sync
+
+    echo -e " Done.\n"
+}
+
+
 create_user() {
     echo -e "Creating User\n"
     sudo adduser --disabled-login --gecos "" $1
@@ -139,39 +163,6 @@ check_network() {
 }
 
 
-uninstall_packages() {
-    echo -n "Uninstalling Packages..."
-    sudo apt-get remove --purge wolfram-engine libreoffice* scratch \
-         minecraft-pi sonic-pi dillo gpicview oracle-java8-jdk \
-         openjdk-7-jre oracle-java7-jdk openjdk-8-jre vim-tiny -y
-    echo -e " Done.\n"
-}
-
-
-update_packages() {
-    echo -n "Updating Packages..."
-    sudo apt-get update && \
-    sudo apt-get upgrade -y && \
-    sudo apt-get dist-upgrade -y && \
-    sudo apt-get autoremove -y && \
-    sudo apt-get autoclean &&
-    sync
-    echo -e " Done.\n"
-}
-
-
-install_packages() {
-    echo -n "Installing Packages..."
-    sudo apt-get install -y --no-install-recommends vim matchbox xserver-xorg \
-                        xserver-xorg-legacy x11-xserver-utils \
-                        ttf-mscorefonts-installer xinit xwit sqlite3 \
-                        libnss3 chromium-browser git
-    sudo apt-get autoremove -y && sudo apt-get autoclean && sync
-
-    echo -e " Done.\n"
-}
-
-
 run_read_only_fs() {
     RO_SCRIPT_URL="https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/read-only-fs.sh"
     wget ${RO_SCRIPT_URL} -O /home_ro/${MAINT_USER}/read-only-fs.sh
@@ -201,65 +192,28 @@ EOF
 }
 
 
-setup_kiosk() {
-    echo -n "Setting up Kiosk..."
-    # Patch Config Files
-    sudo ./update_config.py
-    sudo ./update_rclocal.py
-    sudo ./update_XWrapper.py
-
-    # Create Cron Jobs (Not used with Read Only setup)
-    # crontab cron/pi.cron
-    # sudo crontab cron/root.cron
-
-    # Clean up home
-    rm -rf /home/${APP_USER}/.config
-    rm -rf /home/${APP_USER}/.cache
-    rm -rf /home/${APP_USER}/.pki
-    rm -rf /home/${APP_USER}/.bash_history
-
-    # Map /home to RAMDisk
-    echo -e "\nMapping RAMDisk..."
-    echo 'tmpfs /home tmpfs nodev,nosuid 0 0' | sudo tee -a /etc/fstab
-
-    # Move /home to /home_ro. This will be copied into the new RAMDisk /home on boot.
-    sudo cp -R /home/pi/${REPO} /home/${APP_USER}/${REPO}
-    sudo mv /home /home_ro
-    sudo mkdir -p /home  # For RAMDisk
-    # sudo mkdir -p /home_ro/${APP_USER}
-    # sudo mkdir -p /home_ro/${MAINT_USER}
-    sudo chown -R ${APP_USER}.${APP_USER} /home_ro/${APP_USER}
-    sudo chown -R ${MAINT_USER}.${MAINT_USER} /home_ro/${MAINT_USER}
-
-    echo -e "Done.\n"
-}
-
-
 main() {
     check_root
     check_network
-    copy_scripts
     set_pause
-    uninstall_packages
-    set_pause
-    update_packages
-    set_pause
-    install_packages
-    set_pause
-    create_user ${APP_USER}
-    set_pause
-    create_user ${MAINT_USER}
-    set_pause
-    set_user_passwords
-    set_pause
-    set_user_sudo ${APP_USER}
-    set_pause
-    set_user_sudo ${MAINT_USER}
-    set_pause
-    create_maint_apps
-    set_pause
-    setup_kiosk
-    set_pause
+#    uninstall_packages
+#    set_pause
+#    update_packages
+#    set_pause
+#    install_packages
+#    set_pause
+#    create_user ${APP_USER}
+#    set_pause
+#    create_user ${MAINT_USER}
+#    set_pause
+#    set_user_passwords
+#    set_pause
+#    set_user_sudo ${APP_USER}
+#    set_pause
+#    set_user_sudo ${MAINT_USER}
+#    set_pause
+#    create_maint_apps
+#    set_pause
     set_keyboard
     set_pause
     set_timezone
@@ -268,15 +222,15 @@ main() {
     set_pause
     set_term
     set_pause
-    disable_pi_user
-    set_pause
+#    disable_pi_user
+#    set_pause
     set_hostname
     set_pause
     set_locale
     set_pause
-    run_read_only_fs
+#    run_read_only_fs
+
 }
 
-
-# Run main
+# Run Main
 main
